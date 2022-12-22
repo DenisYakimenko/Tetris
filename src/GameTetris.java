a575dbbddc0237cf1c84c0c4520ace7cc4eae8c6
DenisYakimenko <dyakimenko@mail.ru>
2022-12-23 02:10:27 +0300

    first commit

---
+++ src/GameTetris.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class GameTetris {
+
+    public static void main(String[] args) {
+        new GameTetris().go();
+    }
+
+    void go(){
+
+    }
+
+}
