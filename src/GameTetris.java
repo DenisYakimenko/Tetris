917a6cd149b6f1e9f7bc44398b3e30d431544896
DenisYakimenko <dyakimenko@mail.ru>
2023-01-20 01:23:25 +0300

    ADD end commit

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -3,22 +3,22 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-public class GameTetris {
+class GameTetris extends JFrame{
 
     final String TITLE_OF_PROGRAM = "Tetris";
     final int BLOCK_SIZE = 25;
     final int ARC_RADIUS = 6;
     final int FIELD_WIDTH = 10; // in block
     final int FIELD_HEIGHT = 18;
-    final int START_LOCATION_X = 600;
-    final int START_LOCATION_Y = 180;
+    final int START_LOCATION = 180;
+    //final int START_LOCATION_Y = 180;
     final int FIELD_DX = 7;
     final int FIELD_DY = 26;
     final int LEFT = 37;
     final int UP = 38;
     final int RIGHT = 39;
     final int DOWN = 40;
-    final int SHOW_DELAY = 350; //задержка анимации
+    final int SHOW_DELAY = 200; //задержка анимации
     final int [][][] SHAPES = {
             {{0,0,0,0}, {1,1,1,1}, {0,0,0,0}, {0,0,0,0}, {4, 0x00f0f0}}, // I
             {{0,0,0,0}, {0,1,1,0}, {0,1,1,0}, {0,0,0,0}, {4, 0xf0f000}}, // O
@@ -31,10 +31,10 @@ public class GameTetris {
     };
 
     final int [] SCORES = {100, 300, 700, 1500};
-    int gameScores = 0; // очки игры
+    int gameScore = 0; // очки игры
     int [][] mine = new int[FIELD_HEIGHT+1][FIELD_WIDTH];
     JFrame frame;
-    Canvas canvasPanel = new Canvas();
+    Canvas canvas = new Canvas();
     Random random = new Random();
     Figure figure = new Figure();
     boolean gameOver = false;
@@ -59,59 +59,83 @@ public class GameTetris {
         new GameTetris().go();
     }
 
-    void go(){
-        frame = new JFrame(TITLE_OF_PROGRAM);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //позволяет завершать программу нажатием на крестик
-        frame.setSize(FIELD_WIDTH*BLOCK_SIZE+FIELD_DX, FIELD_HEIGHT*BLOCK_SIZE+FIELD_DY);
-        frame.setLocation(START_LOCATION_X, START_LOCATION_Y);
-        frame.setResizable(false); // запрет на изменение размера окна
-        canvasPanel.setBackground(Color.black);
+    GameTetris() {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //позволяет завершать программу нажатием на крестик
+        //setSize(FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
+        //setLocation(START_LOCATION, START_LOCATION);
+        setBounds(START_LOCATION, START_LOCATION, FIELD_WIDTH * BLOCK_SIZE + FIELD_DX,
+                FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
+        setResizable(false); // запрет на изменение размера окна
+        canvas.setBackground(Color.black);
 
-        frame.addKeyListener(new KeyAdapter() {
-                                 public void keyPressed(KeyEvent e) {
-                                     if (!gameOver) {
-                                         if (e.getKeyCode() == DOWN) figure.drop();
-                                         if (e.getKeyCode() == UP) figure.rotate();
-                                         if (e.getKeyCode() == LEFT || e.getKeyCode() == RIGHT) figure.move(e.getKeyCode());
-                                     }
-                                     canvasPanel.repaint();
-                                 }
-                             });
+        addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (!gameOver) {
+                    if (e.getKeyCode() == DOWN) figure.drop();
+                    if (e.getKeyCode() == UP) figure.rotate();
+                    if (e.getKeyCode() == LEFT || e.getKeyCode() == RIGHT) figure.move(e.getKeyCode());
+                }
+                canvas.repaint();
+            }
+        });
 
 
-           frame.getContentPane().add(BorderLayout.CENTER, canvasPanel);
-           frame.setVisible(true);
+        add(BorderLayout.CENTER, canvas);
+        setVisible(true);
 
-           Arrays.fill(mine[FIELD_HEIGHT], 1);
+        Arrays.fill(mine[FIELD_HEIGHT], 1);
+    }
+
+          void go() {
 
            while (!gameOver){
                try{
                    Thread.sleep(SHOW_DELAY);
                }catch (Exception e){e.printStackTrace();}
-               canvasPanel.repaint();
+               canvas.repaint();
+               checkFilling();
                if(figure.isTouchGround()){
                    figure.leaveOnTheGround();
-                   checkFilling();
                    figure = new Figure();
                    gameOver = figure.isCrossGround();
-               }else{
+               }else
                    figure.stepDown();
                }
            }
 
-    }
+
 
     // метод проверяет заполнились ли какие нибудь линии
     void checkFilling(){
 
+        int row = FIELD_HEIGHT - 1;
+        int countFillRows = 0;
+        while (row > 0) {
+            int filled = 1;
+            for (int col = 0; col < FIELD_WIDTH; col++)
+                filled *= Integer.signum(mine[row][col]);
+            if (filled > 0) {
+                countFillRows++;
+                for (int i = row; i > 0; i--) System.arraycopy(mine[i-1], 0, mine[i], 0, FIELD_WIDTH);
+            } else
+                row--;
+        }
+        if (countFillRows > 0) {
+            gameScore += SCORES[countFillRows - 1];
+            setTitle(TITLE_OF_PROGRAM + " : " + gameScore);
+        }
     }
 
     class Figure{
 
         private ArrayList<Block> figure = new ArrayList<Block>();
-        private int [][] shape = new int [4][4];
-        private int type, size, color;
-        private int x = 3, y = 0;
+        private  int [][] shape = new int [4][4];
+        private  int type;
+        private  int size;
+        private  int color;
+        private  int x = 3;
+        private int y = 0;
 
         Figure(){
             type = random.nextInt(SHAPES.length);
@@ -131,18 +155,37 @@ public class GameTetris {
 
         boolean isTouchGround(){
 
-            for (Block block:figure) if(mine[block.getX()+1][block.getX()]>0) return true;
+            for (Block block:figure) if(mine[block.getY() + 1][block.getX()] > 0) return true;
 
             return false;
         }
 
         boolean isCrossGround(){
 
+            for (Block block : figure) if (mine[block.getY()][block.getX()] > 0) return true;
+
             return false;
         }
 
         void leaveOnTheGround(){
-            for (Block block:figure) mine[block.getX()][block.getX()] = color;
+            for (Block block:figure) mine[block.getY()][block.getX()] = color;
+
+        }
+
+        boolean isTouchWall(int direction) {
+            for (Block block : figure) {
+                if (direction == LEFT && (block.getX() == 0 || mine[block.getY()][block.getX() - 1] > 0)) return true;
+                if (direction == RIGHT && (block.getX() == FIELD_WIDTH - 1 || mine[block.getY()][block.getX() + 1] > 0)) return true;
+            }
+            return false;
+        }
+
+        void move(int direction){
+            if (!isTouchWall(direction)) {
+                int dx = direction - 38; // LEFT = -1, RIGHT = 1
+                for (Block block : figure) block.setX(block.getX() + dx);
+                x += dx;
+            }
 
         }
 
@@ -153,22 +196,60 @@ public class GameTetris {
         }
 
         void drop(){
-
+            while (!isTouchGround()) stepDown();
         }
 
-        void move(int direction){
 
+          boolean isWrongPosition() {
+              for (int x = 0; x < size; x++)
+                  for (int y = 0; y < size; y++)
+                      if (shape[y][x] == 1) {
+                          if (y + this.y < 0) return true;
+                          if (x + this.x < 0 || x + this.x > FIELD_WIDTH - 1) return true;
+                          if (mine[y + this.y][x + this.x] > 0) return true;
+                      }
+              return false;
+          }
+
+        void rotateShape(int direction) {
+            for (int i = 0; i < size/2; i++)
+                for (int j = i; j < size-1-i; j++)
+                    if (direction == RIGHT) { // clockwise
+                        int tmp = shape[size-1-j][i];
+                        shape[size-1-j][i] = shape[size-1-i][size-1-j];
+                        shape[size-1-i][size-1-j] = shape[j][size-1-i];
+                        shape[j][size-1-i] = shape[i][j];
+                        shape[i][j] = tmp;
+                    } else { // counterclockwise
+                        int tmp = shape[i][j];
+                        shape[i][j] = shape[j][size-1-i];
+                        shape[j][size-1-i] = shape[size-1-i][size-1-j];
+                        shape[size-1-i][size-1-j] = shape[size-1-j][i];
+                        shape[size-1-j][i] = tmp;
+                    }
         }
 
         void rotate(){
+            rotateShape(RIGHT);
+            if (!isWrongPosition()) {
+                figure.clear();
+                createFromShape();
+            } else
+                rotateShape(LEFT);
 
         }
+
+        private void createFromShape() {
+        }
+
 
         void paint(Graphics g){
             for (Block block:figure)block.paint(g,color);
         }
 
     }
+
+
 
     class Block{
         private int x, y;
@@ -180,17 +261,17 @@ public class GameTetris {
 
 
 
-        public void setX(int x) {
+        void setX(int x) {
             this.x = x;
         }
-        public void setY(int y) {
+        void setY(int y) {
             this.y = y;
         }
 
-        public int getY() {
+        int getY() {
             return y;
         }
-        public int getX() {
+        int getX() {
             return x;
         }
 
@@ -200,23 +281,36 @@ public class GameTetris {
                     ARC_RADIUS);
         }
 
-
     }
 
-    public class Canvas extends JPanel {
+    class Canvas extends JPanel {
         @Override
         public void paint(Graphics g) {
             super.paint(g);
             for (int x = 0; x<FIELD_WIDTH; x++)
-                for(int y = 0; y < FIELD_HEIGHT; y++)
-                    if (mine[y][x]>0){
-                        g.setColor(new Color(mine[y][x]));
-                        g.fill3DRect(x*BLOCK_SIZE+1, y*BLOCK_SIZE+1, BLOCK_SIZE-1,
-                                BLOCK_SIZE-1, true);
+                for(int y = 0; y < FIELD_HEIGHT; y++){
+                    if (x < FIELD_WIDTH - 1 && y < FIELD_HEIGHT - 1) {
+                        g.setColor(Color.lightGray);
+                        g.drawLine((x+1)*BLOCK_SIZE-2, (y+1)*BLOCK_SIZE, (x+1)*BLOCK_SIZE+2, (y+1)*BLOCK_SIZE);
+                        g.drawLine((x+1)*BLOCK_SIZE, (y+1)*BLOCK_SIZE-2, (x+1)*BLOCK_SIZE, (y+1)*BLOCK_SIZE+2);
                     }
-            figure.paint(g);
+
+                    if (mine[y][x]>0) {
+                        g.setColor(new Color(mine[y][x]));
+                        g.fill3DRect(x * BLOCK_SIZE + 1, y * BLOCK_SIZE + 1, BLOCK_SIZE - 1,
+                                BLOCK_SIZE - 1, true);
+                    }
+                }
+            if(gameOver){
+
+                g.setColor(Color.white);
+                for (int y = 0; y < GAME_OVER_MSG.length; y++)
+                    for (int x = 0; x < GAME_OVER_MSG[y].length; x++)
+                        if (GAME_OVER_MSG[y][x] == 1) g.fill3DRect(x*11+18, y*11+160, 10, 10, true);
+            } else
+                figure.paint(g);
         }
-     }
+    }
 }
 
 
